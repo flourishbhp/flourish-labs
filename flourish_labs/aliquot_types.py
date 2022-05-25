271466d9ee2676c9851f8eb52c3f1a4e16be3720
Itumeleng Mosweu <imosweuk@gmail.com>
2022-05-25 16:23:49 +0200

    add plasma store panel

--- flourish_labs/aliquot_types.py
+++ flourish_labs/aliquot_types.py
@@ -6,9 +6,10 @@ bc = AliquotType(name='Buffy Coat', alpha_code='BC', numeric_code='16')
 
 wb = AliquotType(name='Whole Blood', alpha_code='WB', numeric_code='02')
 
-wb.add_derivatives(bc, pl, wb)
-
 ss = AliquotType(name='Stool Sample', alpha_code='SS', numeric_code='46')
 
 dna_pcr = AliquotType(name='DNA PCR', alpha_code='DP', numeric_code='50')
 
+pbmc = AliquotType(name='PBMC', alpha_code='PBMC', numeric_code='31')
+
+wb.add_derivatives(bc, pl, wb, pbmc)
