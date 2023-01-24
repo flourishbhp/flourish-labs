c06c39c2e158420155af0d070e6ea4193cc740e6
Moses Chawawa <moseschawawa@gmail.com>
2023-01-24 08:41:09 +0200

    Changed naming

--- flourish_labs/child_panels.py
+++ flourish_labs/child_panels.py
@@ -81,7 +81,7 @@ child_pl_store_panel = RequisitionPanel(
 
 lithium_heparin_panel = RequisitionPanel(
     name='lithium_heparin',
-    verbose_name='Quantiferon Requisition form',
+    verbose_name='QuantiFERON Requisition Form',
     aliquot_type=wb,
     processing_profile=lithium_heparin_processing
 )
