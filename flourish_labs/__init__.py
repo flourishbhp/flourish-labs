2ca0f0343aaad501a26f32f4b9b2a00ce989c1b8
Moses Chawawa <moseschawawa@gmail.com>
2024-02-05 16:46:01 +0200

    Added a panel to imports

--- flourish_labs/__init__.py
+++ flourish_labs/__init__.py
@@ -1,5 +1,5 @@
 from .caregiver_panels import caregiver_lab_profile, viral_load_panel, cd4_panel
-from .caregiver_panels import hematology_panel, cbc_panel
+from .caregiver_panels import hematology_panel, cbc_panel, breast_milk_pellet_panel
 from .child_panels import dna_pcr_panel, stool_sample_panel, infant_pl_cytokines_panel
 from .child_panels import fasting_insulin_panel, chemistry_panel
 from .child_panels import lead_panel, fbc_panel, fasting_glucose_panel
